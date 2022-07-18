c593c3758ddbc9ac43f648b689bca4e195172cf0
chenbotao <540387813@qq.com>
2022-07-18 21:28:50 +0800

    second commit

--- src/main/java/com/chen/git/GitTest.java
+++ src/main/java/com/chen/git/GitTest.java
@@ -8,5 +8,6 @@ package com.chen.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git2");
     }
 }
