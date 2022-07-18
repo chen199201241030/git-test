fe94d88ad567475cd96415aa7615d1540120d29f
chenbotao <540387813@qq.com>
2022-07-18 21:24:53 +0800

    frist commit

---
+++ src/main/java/com/chen/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.chen.git;
+
+/**
+ * @author
+ * @company
+ * @creat 2022-07-18 21:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
